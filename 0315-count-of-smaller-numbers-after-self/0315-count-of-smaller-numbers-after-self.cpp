a0e8b6c8540404c09b52925b34bad908fe3e5f6e
Bakshish Kaur <bakshishh.kaur@gmail.com>
2026-07-10 16:39:04 +0530

    Time: 1058 ms (9.44%), Space: 517.4 MB (7.29%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    void mergeSort(vector<pair<int,int>> &arr, int left, int right)
+    {
+        if(left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        vector<pair<int,int>> temp;
+
+        int i = left;
+        int j = mid + 1;
+        int rightCount = 0;
+
+        while(i <= mid && j <= right)
+        {
+            if(arr[i].first <= arr[j].first)
+            {
+                ans[arr[i].second] += rightCount;
+                temp.push_back(arr[i]);
+                i++;
+            }
+            else
+            {
+                rightCount++;
+                temp.push_back(arr[j]);
+                j++;
+            }
+        }
+
+        while(i <= mid)
+        {
+            ans[arr[i].second] += rightCount;
+            temp.push_back(arr[i]);
+            i++;
+        }
+
+        while(j <= right)
+        {
+            temp.push_back(arr[j]);
+            j++;
+        }
+
+        for(int k = left; k <= right; k++)
+        {
+            arr[k] = temp[k - left];
+        }
+    }
+
+    vector<int> countSmaller(vector<int>& nums) {
+
+        int n = nums.size();
+
+        ans.assign(n, 0);
+
+        vector<pair<int,int>> arr;
+
+        for(int i = 0; i < n; i++)
+        {
+            arr.push_back({nums[i], i});
+        }
+
+        mergeSort(arr, 0, n - 1);
+
+        return ans;
+    }
+};
