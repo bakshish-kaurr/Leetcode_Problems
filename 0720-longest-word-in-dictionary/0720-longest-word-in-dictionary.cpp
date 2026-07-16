7d97d34eba2787a6693f20a30b9fbe853ddd5c18
Bakshish Kaur <bakshishh.kaur@gmail.com>
2026-07-16 16:43:11 +0530

    Time: 2592 ms (5.02%), Space: 524 MB (5.03%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.cpp
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
+    bool build(string str,unordered_map<string,int> &mp)
+    {
+        if(str.empty())
+        return true;
+        if(mp.find(str)==mp.end())
+        return false;
+        for(int i=0;i<str.size();i++)
+        {
+            string temp=str;
+            temp.erase(i,1);
+            if(build(temp,mp))
+            return true;
+        }
+        return false;
+    }
     string longestWord(vector<string>& words) {
-        sort(words.begin(), words.end());
-        unordered_set<string> built;
-        string res;
-        for (string w : words) {
-            if (w.size() == 1 || built.count(w.substr(0, w.size() - 1))) {
-                res = w.size() > res.size() ? w : res;
-                built.insert(w);
+        sort(words.begin(),words.end());
+        unordered_map<string,int>mp;
+        for(auto word: words)
+        {
+            mp[word]++;
+        }
+        int maxLen=0;
+        string ans="";
+        for(auto word: words)
+        {
+            if(build(word,mp))
+            {
+                if(word.size()>maxLen)
+                {
+                    ans=word;
+                    maxLen=word.size();
+                }
             }
         }
-        return res;
+        return ans;
     }
 };
